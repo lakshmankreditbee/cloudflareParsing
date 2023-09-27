666bbf00b86687bba15fd6b63a734925a3394966
Sree Lakshman <135098941+lakshmankreditbee@users.noreply.github.com>
2023-09-27 18:49:51 +0530

    Print individual configs

--- CloudflareParse/ViewController.swift
+++ CloudflareParse/ViewController.swift
@@ -10,6 +10,21 @@ import Alamofire
 
 class ViewController: UIViewController {
     
+    private let WEBVIEW_ERROR = "webview_error_config"
+    private let LOGIN = "login_config"
+    private let LIVENESS = "liveness_config"
+    private let LOCALE = "locale_config"
+    private let URLS = "urls"
+    private let EVENTS = "events_config"
+    private let EAADHAAR = "eaadhaar_config"
+    private let CONSENT = "consent_config"
+    private let VKYC = "vkyc_config"
+    private let LOCATION = "location_config"
+    private let PDDB = "pddb_config"
+    private let ENGG = "engg_urls"
+    private let FEATURE = "feature_config"
+    private let LOGS = "logs_config"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchDataFromAPI { result in
@@ -18,9 +33,96 @@ class ViewController: UIViewController {
                 let responseJSON = CFUtils.shared.parseConfigs(data: responseString)
                 let dictionaryResp = responseJSON.dictionaryValue
                 for key in dictionaryResp.keys.sorted() {
-                    print(key)
+                    switch(key) {
+                    case self.WEBVIEW_ERROR:
+                        print("--------WEBVIEW_ERROR------")
+                        if let webviewErrorConfig = dictionaryResp[self.WEBVIEW_ERROR] {
+                            print(webviewErrorConfig)
+                        }
+                        
+                    case self.LOGIN:
+                        print("--------LOGIN---------")
+                        if let loginConfig = dictionaryResp[self.LOGIN] {
+                            print(loginConfig)
+                        }
+                        
+                        
+                    case self.LIVENESS:
+                        print("--------LOGIN--------")
+                        if let livenessConfig = dictionaryResp[self.LIVENESS] {
+                            print(livenessConfig)
+                        }
+                        
+                    case self.LOCALE:
+                        print("--------LOCALE--------")
+                        if let localeConfig = dictionaryResp[self.LOCALE] {
+                            print(localeConfig)
+                        }
+                        
+                    case self.URLS:
+                        print("--------URLS--------")
+                        if let urlsConfig = dictionaryResp[self.URLS] {
+                            print(urlsConfig)
+                        }
+                    
+                    case self.EVENTS:
+                        print("--------EVENTS--------")
+                        if let eventsConfig = dictionaryResp[self.EVENTS] {
+                            print(eventsConfig)
+                        }
+                        
+                    case self.EAADHAAR:
+                        print("--------EAADHAAR--------")
+                        if let eaadhaarConfig = dictionaryResp[self.EAADHAAR] {
+                            print(eaadhaarConfig)
+                        }
+                        
+                    case self.CONSENT:
+                        print("--------CONSENT--------")
+                        if let consentConfig = dictionaryResp[self.CONSENT] {
+                            print(consentConfig)
+                        }
+                        
+                    case self.VKYC:
+                        print("--------VKYC--------")
+                        if let vkycConfig = dictionaryResp[self.VKYC] {
+                            print(vkycConfig)
+                        }
+                        
+                    case self.LOCATION:
+                        print("--------LOCATION--------")
+                        if let locationConfig = dictionaryResp[self.LOCATION] {
+                            print(locationConfig)
+                        }
+                        
+                    case self.PDDB:
+                        print("--------PDDB--------")
+                        if let pddbConfig = dictionaryResp[self.PDDB] {
+                            print(pddbConfig)
+                        }
+                        
+                    case self.ENGG:
+                        print("--------ENGG--------")
+                        if let enggConfig = dictionaryResp[self.ENGG] {
+                            print(enggConfig)
+                        }
+                    
+                    case self.FEATURE:
+                        print("--------FEATURE--------")
+                        if let featureConfig = dictionaryResp[self.FEATURE] {
+                            print(featureConfig)
+                        }
+                        
+                    case self.LOGS:
+                        print("--------LOGS--------")
+                        if let logsConfig = dictionaryResp[self.LOGS] {
+                            print(logsConfig)
+                        }
+                    
+                    default:
+                        break
+                    }
                 }
-                print(responseJSON)
             case .failure(let error):
                 print("Error: \(error)")
                 // Handle the error case here
